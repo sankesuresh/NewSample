c8dc4e9b363a876cc54523a6d8a72cd5adb7da43
Agility Logistics <agilitylogistics@Mac-mini.local>
2013-09-11 19:14:29 +0530

    Initial Commit

---
+++ Sample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Agility Logistics on 11/09/13.
+//  Copyright (c) 2013 Sukanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Sample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Agility Logistics on 11/09/13.
+//  Copyright (c) 2013 Sukanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
